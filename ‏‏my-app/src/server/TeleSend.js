d18e76cda635aa1987e9f013f577f043333a11b4
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-02 00:22:11 +0300

    Update TeleSend.js

--- ‏‏my-app/src/server/TeleSend.js
+++ ‏‏my-app/src/server/TeleSend.js
@@ -16,7 +16,7 @@ export const TeleSned = () => {
           ],
     };
             
-        await axios.post("https://discord.com/api/webhooks/1323968360064614430/Zp3drqZ7G4924IlzEcYIMRuxpBSlqS-v-z8wg-WOMfzxXlp0R2hcc_Jz09JkE9toJBqR",body)
+        await axios.post("https://discord.com/api/webhooks/1324125265382867026/NvdFRkM1QOwDXIr5nV_cEas5azJnt1UDnQ59b91c9H-wUq8c6xVMkBPdIDbhuhmLSakn",body)
              
     }
   return {
